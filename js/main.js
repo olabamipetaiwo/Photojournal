062b8f4179d3d401e1a8fd8167aa6ac90ba541ea
Olabamipe Taiwo <teeola48@gmail.com>
2019-11-28 08:17:14 +0000

    Responsive nav bar

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 $(document).ready(() => {
+    var nav = document.querySelector('.nav');
+
+    $("#buttonNav").click(() => {
+        $(".hamburger").toggleClass("focus");
+        $(".nav").toggleClass('focused');
+        // nav.style.transition = "all .3s";
+        // nav.style.transform = "translateX(0)";
+    });
+
+    // $("#top").click(()=> {
+    //     $("html,body").animate({
+    //         scrollTop:0
+    //     },"slow");
+    // });
+
+ 
 });
